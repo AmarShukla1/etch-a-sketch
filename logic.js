0cfa2f28c904235484500fa4b9d883b31f812306
AmarShukla1 <spamfreemenow12345@gmail.com>
2021-10-07 19:05:34 +0530

    some things left

--- logic.js
+++ logic.js
@@ -17,15 +17,15 @@ function generate(n){
     document.body.appendChild(parent);
     parent.className='container';
     parent.id='box';
- 
-       
+    const block_area=(440*440)/(n*n);
+    const block_width=Math.sqrt(block_area);  
 for (let row = 0; row < n; row++) {
   const rows = document.createElement("div");
   for (let i = 0; i < n; i++) {
     const blocks = document.createElement("div");
-    blocks.style.border = "inset #e1f2f1";
-    blocks.style.width='10px';
-    blocks.style.height='10px';
+    //blocks.style.border = "inset #e1f2f1";
+    blocks.style.width=`${block_width}px`;
+    blocks.style.height=`${block_width}px`;
     blocks.addEventListener("mouseover", function () {
       let str=color();  
       blocks.style.backgroundColor = str;
@@ -41,8 +41,13 @@ for (let row = 0; row < n; row++) {
 const resetting=document.querySelector('button.reset');
 
 resetting.addEventListener('click',function(){
+    
     let n = prompt("enter dimension less than 100");
+    console.log(typeof(n));
+    if(n <=0 || n>=100){alert("Enter correct value");}
+    else{
     document.getElementById('box').remove();
     generate(n);
     resetting.textContent='Click to reset'
+    }
 });
